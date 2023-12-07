33b67976acd15d2d8c6058bed67cdff162f1aa49
kaihhunter <105660821+kaihhunter@users.noreply.github.com>
2023-12-07 11:32:33 -0500

    Update login.php

--- login.php
+++ login.php
@@ -5,3 +5,36 @@
  * File: login.php
  * Description:
  */
+//code from database.php
+require_once('includes/database.php');
+
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+//variable login status.
+$_SESSION['login_status'] = 2;
+
+//initialize variables for username and password
+$username = $passcode = "";
+
+//username and password 
+if (isset($_POST['username']))
+    $username = $conn->real_escape_string(trim($_POST['username']));
+
+if (isset($_POST['password']))
+    $password = $conn->real_escape_string(trim($_POST['password']));
+
+//validate username and password
+
+$sql = "SELECT * FROM users WHERE username= '$username='$username AND password= $password";
+$query= $conn->query($sql);
+if ($query->num_row) {
+$row =$query-> fetch_assoc();
+  $_SESSION['login] = $username;
+  $_SESSION['role'] =$row['role];
+  $_SESSION['name] =$row['firstname'];
+  $_SESSION['login_status] = 1;
+
+}
